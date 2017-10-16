6b76aa5615f4a2eb3fe29364677cb17a5d580e4e
liuqb566 <liuqb566@gmail.com>
2017-10-16 21:05:12 +0800

    correct

--- blog.sh
+++ blog.sh
@@ -3,12 +3,12 @@
 # Program;
 #	this progam is used to push blog.
 # history;
-# 	2016-12-11 gossie first release
+# 	2017-10-16 gossie V2
 
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 
- cd ~/github/liuqb566.github.io || exit 0  
+ cd ~/workspace/liuqb566.github.io || exit 0  
 
 # checkout branch to source
 git checkout source || exit 0 
@@ -19,7 +19,23 @@ git checkout source || exit 0
  read -p 'Please input the information about this commit:' info
  git commit -m '$info'
 
- # cp _sit to another dir
- rm -rf /tmp/site/*
- cp -r -a ./_site/* /tmp/site/
+ #checkout branch to master and checkout files of _site of source to master
+ git checkout master || exit 0
+ rm -r *
+ git checkout source _site ||exit 0
+ mv _site/* .
+ rm -r _site
+
+ #add and commit
+ git add -A
+ read -p 'Please input the information about this commit:' info
+ git commit -m '$info' || exit 0
+
+ # push
+ git push --all ||exit 0
+ 
+ echo "All is ok!"
+
+ 
+
 
